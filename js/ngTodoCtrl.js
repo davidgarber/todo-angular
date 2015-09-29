e93aa1485b494d3c54d67c3130915eec388bb354
David Garber <davidgarber@gmail.com>
2015-09-29 11:45:38 -0700

    Add new task to ngTodoCtrl.js

--- js/ngTodoCtrl.js
+++ js/ngTodoCtrl.js
@@ -5,7 +5,10 @@ ngTodo.controller('NgTodoCtrl', function NgTodoCtrl($scope) {
     { val: "A newer cool thing", completed: true},
     { val: "A newer cooler thing", completed: false},
     { val: "A new coolest thing", completed: false}
-
-
   ];
+
+  $scope.addNewTask = function() {
+    $scope.todos.unshift({completed: false, val: $scope.newTask});
+    $scope.newTask = "";
+  }
 });
