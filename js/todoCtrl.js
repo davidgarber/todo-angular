e0bd120b06d8cbb8ea905e5ea76139eb37fd1d77
David Garber <davidgarber@gmail.com>
2015-09-29 11:16:01 -0700

    Create todo.ctrl.

---
+++ js/todoCtrl.js
@@ -0,0 +1,11 @@
+todo.controller('TodoCtrl', function TodoCtrl($scope) {
+  $scope.todos = [
+    { val: "A new cool thing", completed: false},
+    { val: "A new cooler thing", completed: true},
+    { val: "A newer cool thing", completed: true},
+    { val: "A newer cooler thing", completed: false},
+    { val: "A new coolest thing", completed: false}
+
+
+  ];
+});
